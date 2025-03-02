0fab357047a675013e66979729606f8551deac06
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-03-02 14:19:31 +0900

    Implementing Dynamic Programming

--- Algorithm/DynamicProgramming/DynamicProgramming(Andrei).js
+++ Algorithm/DynamicProgramming/DynamicProgramming(Andrei).js
@@ -1,18 +1,48 @@
-function memoizedAddTo80() {
-	let cache = {};
+// function memoizedAddTo80() {
+// 	let cache = {};
 
-	return function (n) {
+// 	return function (n) {
+// 		if (n in cache) {
+// 			return cache[n];
+// 		} else {
+// 			console.log("long time");
+// 			cache[n] = n + 80;
+// 			return cache[n];
+// 		}
+// 	};
+// }
+
+// const memoized = memoizedAddTo80();
+// console.log("1", memoized(5));
+// console.log("2", memoized(5));
+
+let calc = 0;
+function fibonacci(n) {
+	calc++;
+	if (n < 2) {
+		return n;
+	}
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function fibonacciMaster() {
+	let cache = {};
+	return function fib(n) {
+		calc++;
 		if (n in cache) {
 			return cache[n];
 		} else {
-			console.log("long time");
-			cache[n] = n + 80;
-			return cache[n];
+			if (n < 2) {
+				return n;
+			} else {
+				cache[n] = fib(n - 1) + fib(n - 2);
+				return cache[n];
+			}
 		}
 	};
 }
 
-const memoized = memoizedAddTo80();
+const fasterFib = fibonacciMaster();
 
-console.log("1", memoized(5));
-console.log("2", memoized(5));
+console.log(fasterFib(35));
+console.log(calc); // 46368

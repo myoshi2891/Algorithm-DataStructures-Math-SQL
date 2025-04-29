2494f1c5e8f148e53b9894170aceef3b9db16c47
myoshizumi <myoshizumi@alumni.berklee.edu>
2025-04-29 12:06:01 +0900

    at coder 繰り返し二乗法 A28, A29

---
+++ Algorithm/Other/at coder/A29.js
@@ -0,0 +1,37 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+const MOD = 1000000007n; // BigIntのnを付ける
+
+// BigInt版高速累乗
+function modPow(a, b, mod) {
+	let result = 1n;
+	a = BigInt(a) % mod;
+	b = BigInt(b);
+
+	while (b > 0) {
+		if (b % 2n === 1n) {
+			result = (result * a) % mod;
+		}
+		a = (a * a) % mod;
+		b = b / 2n;
+	}
+
+	return result;
+}
+
+let input = [];
+rl.on("line", (line) => {
+	input = line.trim().split(" ");
+	rl.close();
+});
+
+rl.on("close", () => {
+	const a = input[0];
+	const b = input[1];
+	console.log(modPow(a, b, MOD).toString()); // 出力は文字列に変換
+});
